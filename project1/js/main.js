89b8d5c0fd69524958261a5caeee483af82dbeeb
LonelyMiko <LonelyMiko@users.noreply.github.com>
2020-06-14 19:54:03 +0300

    Created Project1
    Used: HTML/CSS/JS Bootstrap 4 and jQuery

---
+++ project1/js/main.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+      $('#header').addClass('header-fixed');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+      $('#header').removeClass('header-fixed');
+    }
+  });
