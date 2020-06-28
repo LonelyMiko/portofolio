b573efde7f86be415a0b9724f2bd60e9eaaf5b3e
LonelyMiko <LonelyMiko@users.noreply.github.com>
2020-06-28 12:49:13 +0300

    Collor Flipper using JavaScript
    Used: HTML/JavaScript/CSS

---
+++ Color Flipper/color.js
@@ -0,0 +1,17 @@
+get_random = function (list) {
+  return list[Math.floor((Math.random()*list.length))];
+} 
+
+const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function (argument) {
+	let hexColor = "#";
+	for (let i = 0; i < 6; i++) {
+		hexColor += get_random(hex);
+	}
+	color.textContent = hexColor;
+	document.body.style.backgroundColor = hexColor;
+})
+
